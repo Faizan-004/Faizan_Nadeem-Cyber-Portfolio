7309696ced7844e6b764a78464733d2e91b04bc0
Faizan <faizan.nadeem@uconn.edu>
2026-01-25 21:55:24 -0500

    restart app

--- SecureFitnessApp/mobile-app/src/screens/LogWorkoutScreen.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function LogWorkoutScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Log Workout Screen</Text>
-    </View>
-  );
-}
--- SecureFitnessApp/mobile-app/src/screens/WorkoutHistoryScreen.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function WorkoutHistoryScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Workout History Screen</Text>
-    </View>
-  );
-}
